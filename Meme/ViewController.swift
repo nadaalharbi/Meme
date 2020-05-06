080cc916f681b95a002871e9aa58762204d926ee
sy <alharbinada22@gmail.com>
2020-05-06 22:37:59 +0300

    Initial Commit

---
+++ Meme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme
+//
+//  Created by sy on 5/6/20.
+//  Copyright © 2020 sy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
